152dba58c055b383856c1500b44f95972227599f
pchan <pchan@noreply.git.chaoticvermilion.is>
2024-05-29 11:55:01 -0400

    chore: remove unused function + test generatd by Cargo

--- src/lib.rs
+++ src/lib.rs
@@ -41,18 +41,3 @@ pub enum IrisMessage {
     ServerError,
     BadRoomIdentifier,
 }
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
